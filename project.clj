e975933dacd080ac19d0117b73ba90843b004a8e
Emil Bengtsson <emil@emil0r.com>
2022-02-23 10:20:27 +0100

    Up dep on mqtt library

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [com.taoensso/timbre "5.1.2" :scope "provided"]
 
                  ;; MQTT library
-                 [songpark/mqtt "1.0.0-alpha6" :scope "provided"]
+                 [songpark/mqtt "1.0.1-alpha1" :scope "provided"]
 
                  ;; core async. used for TPX IPC/Jam bindings
                  [org.clojure/core.async "1.5.648"]]
